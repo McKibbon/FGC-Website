ec18664f05a9cc7435779e13d561e7cb9ff05e65
Pierson <piersonm@shaw.ca>
2019-04-13 13:12:37 -0600

    Removed sample data.

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 
     var userFeed = new Instafeed({
         get: 'user',
-        userId: '249400917',
+        userId: , //User Info
         limit: 12,
         resolution: 'standard_resolution',
-        accessToken: '249400917.1677ed0.d847c8970a4a4bf38a567e93f0855b89',
+        accessToken: , //User Info 
         sortBy: 'most-recent',
         template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
